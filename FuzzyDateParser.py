6c8ab7b722425d61a8c8dae866fee913d27d1c69
JasonAvina <jason.avina@gmail.com>
2025-06-19 14:11:11 -0700

    getting date parser to work

--- FuzzyDateParser.py
+++ FuzzyDateParser.py
@@ -13,22 +13,23 @@ class FuzzyDateParser:
         self.df['Parsed Return Date'] = self.df.apply(self.parse_row, axis = 1)
         
     # This function will do a few things:
-    # - it will first return the return date if its in the proper format for that row
-    # - if its not in the proper format it will call parse(checkout_date, unparsed_return_date) method
+    # - if row value is in datetime format, then value is returned
+    # - else it will call parse(checkout_date, unparsed_return_date) method
     # This will convert the unparsed_return_date to a parsed_return_date in the proper format. 
     def parse_row(self, row):
-        if isinstance(row['Date Returned'], datetime):
+        if pd.notna(row['Date Returned']):
+            if isinstance(row['Date Returned'], datetime):
+                return row['Date Returned']
+            else:
+                checkout_date = row['Date Checked Out']
+                unparsed_return_date = row['Date Due']
+                parsed_return_date = dateparser.parse(unparsed_return_date, settings={
+                    'RELATIVE_BASE': checkout_date,
+                    'PREFER_DATES_FROM': 'future'
+            })
+            return parsed_return_date
+        else:
             return row['Date Returned']
-        
-        checkout_date = row['Date Checked Out']
-        if isinstance(checkout_date, str):
-            checkout_date = parser.parse(checkout_date)
-        unparsed_return_date = row['Date Due']
-        parsed_return_date = dateparser.parse(unparsed_return_date, settings={
-            'RELATIVE_BASE': checkout_date,
-            'PREFER_DATES_FROM': 'future'
-        })
-        return parsed_return_date
 
 
             
